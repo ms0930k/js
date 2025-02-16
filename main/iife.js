ec9b8db569a13841e0e0a7a14806b41ddca6679a
MOHIT S KAPURIYA <ms0930k@gmail.com>
2025-02-16 11:19:22 +0000

    commit

---
+++ main/iife.js
@@ -0,0 +1,15 @@
+(function hi(name)
+{
+    console.log(`hi ${name}`);
+})("hitesh");
+    
+((name) =>
+{
+    console.log(`hi ${name}`);
+})("mohit")
+let val1 = 10
+let val2 = 5
+function AD(num1, num2){
+    let reus = num1+num2
+    return reus
+}
