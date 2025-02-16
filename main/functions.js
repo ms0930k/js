085dd48deec8fed05244021f716eb1f89c038c46
MOHIT S KAPURIYA <ms0930k@gmail.com>
2025-02-16 09:58:09 +0000

    commit

---
+++ main/functions.js
@@ -0,0 +1,34 @@
+function myf(a,b){
+    // console.log(a+b);
+    return a+b
+}
+console.log(myf(1,2))
+console.log(myf(4534,4564))
+
+function one(){
+    const username = " mohit "
+    function two (){
+        console.log("hi i am mohir" , username);
+        
+    }
+    two()
+}
+one()
+function one(){
+    const username = " mohit "
+    function two (){
+        console.log("hi i am mohir" , username);
+        
+    }
+    two()
+}
+one()
+function one1(){
+    const username = " mohit "
+    function two1 (){
+        console.log("hi i am mohir" , username);
+        
+    }
+    // two1()
+}
+one1()
