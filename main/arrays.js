ab84ab07480550bf2d96c727aabd8161c91c8335
MOHIT S KAPURIYA <ms0930k@gmail.com>
2025-02-16 08:28:51 +0000

    commit to main

---
+++ main/arrays.js
@@ -0,0 +1,14 @@
+let arr = ["shakjtinenfjil","fgikjhdklf","drigjilsdfg"]
+arr.push("hielle")
+console.log(arr);
+arr.unshift("gfhsdifghe0000")
+console.log(arr);
+arr.shift()
+arr.shift()
+console.log(arr);
+console.log(arr.join());
+mys = Symbol("hi")
+let obj = {
+
+}
+console.log(obj);
